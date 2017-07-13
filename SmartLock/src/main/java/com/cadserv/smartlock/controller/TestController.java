bebc07c11c74611d04aa03fbc59105a3ef9cee58
Administrator <Administrator@user-PC>
2017-07-13 17:34:58 +0900

    .

---
+++ SmartLock/src/main/java/com/cadserv/smartlock/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.cadserv.smartlock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String test() {
+		System.out.println("test");
+		return "test";
+	}
+}
