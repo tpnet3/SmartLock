5b2fd7d1cc3ff1bf33c96abbf3ed96300448cd91
Gyeong-Sik Park <tpnet3@gmail.com>
2017-08-10 11:05:17 +0900

    CommonDAO 주석

--- SmartLock/src/smartlock/common/CommonDAO.java
+++ SmartLock/src/smartlock/common/CommonDAO.java
@@ -15,19 +15,19 @@ public class CommonDAO extends SqlSessionDaoSupport{
 	public void setSuperSqlMapClient(SqlSessionTemplate sqlSessionTemplate) throws Exception {
         super.setSqlSessionTemplate(sqlSessionTemplate);
     }
-	
+
 	/**
-	 * ????? ???????? ????? ???
-	 * @param sql 
-	 * @param paramVO ????? ???????? ???? ????? VO
-	 * @return List<Object> ??? ????? ??????
+	 * 목록을 가져오는 기능을 한다
+	 * @param sql
+	 * @param paramVO 목록을 가져오기 위해 필요한 VO
+	 * @return List<Object> 결과 목록을 반환한다
 	 */
 	public List<Object> selectList(String sql, Object paramVO) throws Exception {
 		return getSqlSession().selectList(sql, paramVO);
 	}
 
 	/**
-	 * ???????? Object???·? ??????.
+	 * 상세정보를 Object형태로 반환한다.
 	 * @param sql
 	 * @param paramVO
 	 * @return
@@ -35,32 +35,32 @@ public class CommonDAO extends SqlSessionDaoSupport{
 	public Object selectOne(String sql, Object paramVO) throws Exception {
 		return getSqlSession().selectOne(sql, paramVO);
 	}
-	
+
 	/**
-	 * ????? ?????? ????? ???
+	 * 항목을 등록하는 기능을 한다
 	 * @param sql
 	 * @param paramVO
-	 * @return ?????? null / ???н? ????
+	 * @return 성공시 null / 실패시 에러
 	 */
 	public int insert(String sql, Object paramVO) throws Exception {
 		return getSqlSession().insert(sql, paramVO);
 	}
-	
+
 	/**
-	 * ????? ??????? ????? ???
+	 * 항목을 수정하는 기능을 한다
 	 * @param sql
 	 * @param paramVO
-	 * @return ?????? 1 / ???н? 0
+	 * @return 성공시 1 / 실패시 0
 	 */
 	public int update(String sql, Object paramVO) throws Exception {
 		return getSqlSession().update(sql, paramVO);
 	}
-	
+
 	/**
-	 * ????? ??????? ????? ???
+	 * 항목을 삭제하는 기능을 한다
 	 * @param sql
 	 * @param paramVO
-	 * @return ?????? row???? ??????
+	 * @return 삭제한 row수를 반환한다
 	 */
 	public int delete(String sql, Object paramVO) throws Exception {
 		return getSqlSession().delete(sql, paramVO);
