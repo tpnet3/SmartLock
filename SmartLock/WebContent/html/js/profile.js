cacf324d4a4119b389432f272a790cd573de102d
dongwhy <kd9336w@naver.com>
2017-08-15 13:34:12 +0900

    profile-btn 수정(form,submit)

--- SmartLock/WebContent/html/js/profile.js
+++ SmartLock/WebContent/html/js/profile.js
@@ -1,4 +1,4 @@
-$("#profile-btn").on("click", function() {
+$(".profile-btn").submit(function() {
 	// check validation
 	if($("#email").val() == '') {
 		$("#email").focus();
@@ -23,11 +23,11 @@ $("#profile-btn").on("click", function() {
 		type : "POST",
         contentType: "application/json",
 		dataType : "json",
-		data : {
+		data : JSON.stringify({
 			"id": SmartLock.user.id, // smartlock.id -> SmartLock.user.id 로 수정되었습니다.
 			"email" : $("#email").val(),
 			"phone" : $("#phone").val(),
-		},
+		}),
 		success : function (data){
 			if(data.status == "success") {
 				//회원정보 수정 성공 페이지로 이동(메인페이지 이동버튼 제공)
