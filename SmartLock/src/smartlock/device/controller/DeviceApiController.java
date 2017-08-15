1a86fa540a1370722ac88e9d70440ee0d12d0e4d
Gyeong-Sik Park <tpnet3@gmail.com>
2017-08-15 17:45:19 +0900

    디바이스 삭제 구현

--- SmartLock/src/smartlock/device/controller/DeviceApiController.java
+++ SmartLock/src/smartlock/device/controller/DeviceApiController.java
@@ -3,19 +3,17 @@ package smartlock.device.controller;
 import java.util.ArrayList;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import smartlock.common.vo.DataResVO;
 import smartlock.device.service.DeviceService;
+import smartlock.device.vo.DeviceIdVO;
 import smartlock.device.vo.DeviceVO;
-import smartlock.member.vo.UserVO;
 
 @Controller
 public class DeviceApiController {
@@ -71,6 +69,31 @@ public class DeviceApiController {
 			dataResVO.setData("error");
 		}
 		
+		return dataResVO;
+	}
+
+	@RequestMapping(value = "/device/delete", method = RequestMethod.POST)
+	public @ResponseBody DataResVO deleteDevice(
+			@RequestBody DeviceIdVO deviceIdVO,
+			HttpServletRequest request) {
+		DataResVO dataResVO = new DataResVO();
+
+		try {
+			int deletedRowCnt = deviceService.deleteDevice(deviceIdVO.getId());
+
+			if (deletedRowCnt > 0) {
+				dataResVO.setStatus("success");
+				dataResVO.setData(deletedRowCnt);
+			} else {
+				dataResVO.setStatus("success");
+				dataResVO.setData(null);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			dataResVO.setStatus("error");
+			dataResVO.setData("error");
+		}
+
 		return dataResVO;
 	}
 }
