4ed2abef34cff73333da7700ab4c1b9c8de1816e
JaeHyun <kjh59761@naver.com>
2017-08-22 16:32:40 +0900

    statistics화면

--- SmartLock/WebContent/html/js/statistics.js
+++ SmartLock/WebContent/html/js/statistics.js
@@ -3,26 +3,26 @@ $(document).ready(function(){
 	var getLicense = Math.round($("#getLicense-value").html()/total*10);
 	var demo = Math.round($("#demo-value").html()/total*10);
 	
-	$("#total").children(".progress-left").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0.5s");
-	$("#total").children(".progress-right").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0s");
+	$("#total").children(".progress-left").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0.5s");
+	$("#total").children(".progress-right").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0s");
   
-	$("#license").children(".progress-left").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0.5s");
-	$("#license").children(".progress-right").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0s");
+	$("#license").children(".progress-left").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0.5s");
+	$("#license").children(".progress-right").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0s");
 
 	if(getLicense<6) {
 		$("#getLicense").children(".progress-left").children(".progress-bar").css("animation", "loading-0 0.5s linear forwards 0.5s");
-		$("#getLicense").children(".progress-right").children(".progress-bar").css("animation", "loading-"+(getLicense-4)+" 0.5s linear forwards 0s");
+		$("#getLicense").children(".progress-right").children(".progress-bar").css("animation", "loading-"+(getLicense)+" 0.5s linear forwards 0s");
 	} else {
-		$("#getLicense").children(".progress-left").children(".progress-bar").css("animation", "loading-"+(getLicense-4)+" 0.5s linear forwards 0.5s");
-		$("#getLicense").children(".progress-right").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0s");
+		$("#getLicense").children(".progress-left").children(".progress-bar").css("animation", "loading-"+(getLicense-5)+" 0.5s linear forwards 0.5s");
+		$("#getLicense").children(".progress-right").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0s");
 	}
 	
 	if(demo<6) {
 		$("#demo").children(".progress-left").children(".progress-bar").css("animation", "loading-0 0.5s linear forwards 0.5s");
-		$("#demo").children(".progress-right").children(".progress-bar").css("animation", "loading-"+(demo-4)+" 0.5s linear forwards 0s");
+		$("#demo").children(".progress-right").children(".progress-bar").css("animation", "loading-"+(demo)+" 0.5s linear forwards 0s");
 	} else {
-		$("#demo").children(".progress-left").children(".progress-bar").css("animation", "loading-"+(demo-4)+" 0.5s linear forwards 0.5s");
-		$("#demo").children(".progress-right").children(".progress-bar").css("animation", "loading-6 0.5s linear forwards 0s");
+		$("#demo").children(".progress-left").children(".progress-bar").css("animation", "loading-"+(demo-5)+" 0.5s linear forwards 0.5s");
+		$("#demo").children(".progress-right").children(".progress-bar").css("animation", "loading-5 0.5s linear forwards 0s");
 	}
 	
 	
