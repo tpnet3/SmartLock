576b1a99d56f29d4ad48cf22700bac2258123824
Gyeong-Sik Park <tpnet3@gmail.com>
2017-08-24 15:13:43 +0900

    소푸트웨어 목록 조회 구현

--- SmartLock/src/smartlock/software/vo/SoftwareVO.java
+++ SmartLock/src/smartlock/software/vo/SoftwareVO.java
@@ -3,44 +3,59 @@ package smartlock.software.vo;
 public class SoftwareVO {
 
     private int id;
-    private String name;
+    private String sw_name;
     private int corp_id;
     private String version;
     private String path;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
+
+	public String getSw_name() {
+		return sw_name;
 	}
-	public void setName(String name) {
-		this.name = name;
+
+	public void setSw_name(String sw_name) {
+		this.sw_name = sw_name;
 	}
+
 	public int getCorp_id() {
 		return corp_id;
 	}
+
 	public void setCorp_id(int corp_id) {
 		this.corp_id = corp_id;
 	}
+
 	public String getVersion() {
 		return version;
 	}
+
 	public void setVersion(String version) {
 		this.version = version;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+
 	@Override
 	public String toString() {
-		return "SoftwareVO [id=" + id + ", name=" + name + ", corp_id=" + corp_id + ", version=" + version + ", path="
-				+ path + "]";
+		return "SoftwareVO{" +
+				"id=" + id +
+				", sw_name='" + sw_name + '\'' +
+				", corp_id=" + corp_id +
+				", version='" + version + '\'' +
+				", path='" + path + '\'' +
+				'}';
 	}
-
 }
