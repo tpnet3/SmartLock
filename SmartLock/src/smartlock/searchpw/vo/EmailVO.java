919607448aa2ff8ba1d8ef83926a055ca9394576
kimdongwan <kd9336w@naver.com>
2017-09-16 12:05:20 +0900

    vo, service 추가

---
+++ SmartLock/src/smartlock/searchpw/vo/EmailVO.java
@@ -0,0 +1,20 @@
+package smartlock.searchpw.vo;
+
+public class EmailVO {
+	private String id;
+	private String email;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
