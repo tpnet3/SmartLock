3cfa23e0cdab71aea9ee07538770cbfdc41a6c7f
Gyeong-Sik Park <tpnet3@gmail.com>
2017-09-19 14:38:08 +0900

    최초 화면 수정

--- SmartLock/src/smartlock/web/controller/MainWebController.java
+++ SmartLock/src/smartlock/web/controller/MainWebController.java
@@ -18,11 +18,14 @@ public class MainWebController {
         UserVO userVO = (UserVO) httpSession.getAttribute("user");
 
         if (userVO != null && userVO.getAuthority() == 0) {
-            return new ModelAndView("/smartlock/main_user");
+            //return new ModelAndView("/smartlock/main_user");
+            return new ModelAndView("redirect:/device");
         } else if (userVO != null && userVO.getAuthority() == 1) {
-            return new ModelAndView("/smartlock/main_manager");
+            //return new ModelAndView("/smartlock/main_manager");
+            return new ModelAndView("redirect:/statistics");
         } else {
-            return new ModelAndView("/smartlock/main");
+            //return new ModelAndView("/smartlock/main");
+            return new ModelAndView("redirect:/about_us");
         }
     }
 
